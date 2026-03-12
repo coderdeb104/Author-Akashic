fd8e349aa9e516882e6af62be1456794dd81fc27
Debjit Dey <debjitdey148@gmail.com>
2026-03-12 16:01:52 +0000

    No, not working.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,12 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  // Create a response object that we can modify and return
-  const response = NextResponse.next({
+  let response = NextResponse.next({
     request: {
       headers: request.headers,
     },
   })
 
-  // Create a Supabase client that can read and write cookies
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -19,18 +17,38 @@ export async function middleware(request: NextRequest) {
           return request.cookies.get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
-          // The `set` method is called whenever the GoTrue client
-          // authenticates a user or refreshes their session.
-          // We update the request and response cookies.
-          request.cookies.set({ name, value, ...options })
-          response.cookies.set({ name, value, ...options })
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          })
         },
         remove(name: string, options: CookieOptions) {
-          // The `remove` method is called whenever the GoTrue client
-          // signs out a user.
-          // We update the request and response cookies.
-          request.cookies.set({ name, value: '', ...options })
-          response.cookies.set({ name, value: '', ...options })
+          request.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
         },
       },
     }
@@ -39,7 +57,6 @@ export async function middleware(request: NextRequest) {
   // This will refresh the session if it's expired
   await supabase.auth.getUser()
 
-  // Return the response object with the updated cookies
   return response
 }
 
